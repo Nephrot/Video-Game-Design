e426292b870869bed953198a628bc8e0a50caab9
qadada <krimsonmc@gmail.com>
2018-12-09 12:55:52 -0500

    Basic Movement & Collision Detection
    -Part 2 boys

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour {
+public Rigidbody2D rb;
+
+public Animator animator;
+Vector3 movement;
+public float moveSpeed = 3f;	
+	// Update is called once per frame
+   void Update(){
+    Move();
+   }
+   void Move(){
+    movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+    rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed);    
+    animator.SetFloat("Horizontal", movement.x);
+    animator.SetFloat("Vertical", movement.y);
+    animator.SetFloat("Magnitude", movement.magnitude);
+   }
+}
