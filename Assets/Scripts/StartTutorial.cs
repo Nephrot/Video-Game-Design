6f27c3f324e45eb3f8cfc9e7cf532488ebf9cfc4
Teknical Mage <arcazjordan@gmail.com>
2019-01-02 21:42:53 -0500

    Broken Diff Version
    Disreguard

---
+++ Assets/Scripts/StartTutorial.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartTutorial : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("BasicMovementAndAnim");
+    }
+}
